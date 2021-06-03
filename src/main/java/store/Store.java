543bca22dedaae71ceab46fdd5cd1390dce35091
Vlad <vladbaykin@gmail.com>
2021-06-03 23:56:55 +0300

    Scriplet. Динамическая таблица.

---
+++ src/main/java/store/Store.java
@@ -0,0 +1,26 @@
+package store;
+
+import model.Post;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Store {
+    private static final Store INST = new Store();
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private Store() {
+        posts.put(1, new Post(1, "Junior Java Job", "description...", LocalDateTime.now().minusDays(1)));
+        posts.put(2, new Post(2, "Middle Java Job", "description...", LocalDateTime.now().minusDays(10)));
+        posts.put(3, new Post(3, "Senior Java Job", "description...", LocalDateTime.now().minusDays(15)));
+    }
+    public static Store instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
