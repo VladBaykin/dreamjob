aa032cef852c167652ea8e2fce4d664e8940b22d
Vlad <vladbaykin@gmail.com>
2021-06-04 13:39:18 +0300

    candidates.jsp - список кандидатов.

--- src/main/java/store/Store.java
+++ src/main/java/store/Store.java
@@ -1,5 +1,6 @@
 package store;
 
+import model.Candidate;
 import model.Post;
 
 import java.time.LocalDateTime;
@@ -10,17 +11,25 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Store {
     private static final Store INST = new Store();
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job", "description...", LocalDateTime.now().minusDays(1)));
         posts.put(2, new Post(2, "Middle Java Job", "description...", LocalDateTime.now().minusDays(10)));
         posts.put(3, new Post(3, "Senior Java Job", "description...", LocalDateTime.now().minusDays(15)));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Post> findAllPosts() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }
